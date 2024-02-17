f5dec45de6b705cc52a0699ef9520faf41e40e07
altyx <samir.moutawakil59@gmail.com>
2024-02-17 15:45:33 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <main className={styles.main}>
       <section className={styles.presentation}>
-        <Image src="/banniere3.jpg" alt="banniere" width={1000} height={500} priority={false} style={imageStyle} />
+        <Image src="/banniere3.jpg" alt="banniere"  width={1000} height={500} priority={true} style={imageStyle} />
         <div className={styles.name}>
           <h1>Je suis Samir</h1>
           <div>I&apos;m a full stack web and mobile developer based in Lille, France. I have a passion for creating scallable, robust and fast applications.</div>
@@ -22,30 +22,27 @@ export default function Page() {
         <h2>Derniers projets</h2>
         <div className={styles.projects}>
           <div className={styles.project}>
-            <Image src="/wpc.png" alt="weplaycircular" width={480} height={300} style={imageStyle} />
-            <div className={styles.title}>Décathlon - We Play Circular</div>
-            <div>Une application web permettant de louer du matériel sportif.</div>
+            <Link href="/">
+              <Image src="/wpc.png" alt="weplaycircular" width={480} height={300} style={imageStyle} priority={false} />
+              <div className={styles.title}>Décathlon - We Play Circular</div>
+              <div>Une application web permettant de louer du matériel sportif.</div>
+              <div>ReactJS - NodeJS - TypeScript - Docker - PostgreSQL</div>
+            </Link>
           </div>
           <div className={styles.project}>
-            <Image src="/masteos.png" alt="weplaycircular" width={480} height={300} style={imageStyle} />
-            <div className={styles.title}>Masteos</div>
-            <div className={styles.description}>Une application web et mobile permettant d&apos;investir dans l&apos;immobilier locatif</div>
+            <Link href="/">
+              <Image src="/masteos.png" alt="weplaycircular" width={480} height={300} style={imageStyle}
+                     priority={false}/>
+              <div className={styles.title}>Masteos</div>
+              <div className={styles.description}>Une application web et mobile permettant d&apos;investir dans
+                l&apos;immobilier locatif
+              </div>
+              <div>ReactJS - NodeJS - TypeScript - Docker - MongoDB - GraphQL</div>
+            </Link>
           </div>
         </div>
 
         <Link className={styles.see_all} href="/">Voir tous les projets</Link>
-      </section>
-
-      <section className={styles.skills_section}>
-        <h2>Compétences</h2>
-        <div className={styles.skills}>
-          <div className={styles.skill}>React</div>
-          <div className={styles.skill}>NodeJS</div>
-          <div className={styles.skill}>JavaScript</div>
-          <div className={styles.skill}>TypeScript</div>
-          <div className={styles.skill}>React native</div>
-        </div>
-        <Link className={styles.see_all} href="/">Voir toutes les compétences</Link>
       </section>
 
       <footer className={styles.footer}>
